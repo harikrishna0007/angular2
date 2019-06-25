b248b9e626a35199d8361d30d7f6cadd26fc5a86
HARI KRISHNA <eharikrishna007@gmail.com>
2019-06-25 17:47:27 +0530

    task2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    BannerComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+  newArray = [{
+    headding: "For those about to rock...",
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.",
+    image: "01.jpg"
+  },
+  {
+    headding: "We salute you!",
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.",
+    image: "02.jpg"
+  },
+  {
+    headding: "Let there be rock!",
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.",
+    image: "03.jpg"
+  }
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
